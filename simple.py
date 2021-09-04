767e46b299e83f81eec0dd0543afd067bef8c740
Ruslan Gorokhov <rus.gorokhov@mail.ru>
2021-09-04 18:08:56 +0300

    First commit

---
+++ simple.py
@@ -0,0 +1,16 @@
+# job = {'city': "Москва", 'temperature': '20'}
+# print(job['city'])
+# temp = int(job['temperature'])
+# print(temp - 5)
+
+def get_sum(one, two, delimiter='&'):
+   refund =  f'{one} & {two}'
+   return refund
+
+# print(get_sum(10000000, 2002020202002))
+# print(get_sum(100, 110))
+# x = get_sum('one', 'two')
+# b = get_sum(100, 110)
+# print(x + b)
+
+
