435a5768410a2e424c062f49cd1d163171a15ce0
Ruslan Gorokhov <rus.gorokhov@mail.ru>
2021-09-08 14:59:41 +0300

    Выполнил первое задание цикл for

--- lesson_for.py
+++
@@ -1,4 +0,0 @@
-user_name = input('Введите Имя: ')
-
-for last_name in user_name:
-    print(last_name)
---
+++ lesson_for1.py
@@ -0,0 +1,8 @@
+# Задание 1 по for
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for score in range(len(number)):
+    number[score] += 1
+print(number)
+
+
