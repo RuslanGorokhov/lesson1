47c7214aebc4cf7f5402a1e0c736a1453aee77bf
Ruslan Gorokhov <rus.gorokhov@mail.ru>
2021-09-08 14:40:51 +0300

    Выполнил первое задание цикл for

--- lesson_if2.py
+++ lesson_if2.py
@@ -1,1 +1,8 @@
-print('xxxxx')
+# Задание 2_if
+def strings_checkin(string12, string2):
+    if type(string12) == str and type(string2) == str:
+        return 1
+    else:
+        return 2
+
+print(strings_checkin('name1', 'name2'))
