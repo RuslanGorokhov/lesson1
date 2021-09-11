720e612a7e96a4335e3233d1ed517f22e04e09c0
Ruslan Gorokhov <rus.gorokhov@mail.ru>
2021-09-11 18:10:19 +0300

    Выполнил второе задание if2

--- lesson_if2.py
+++ lesson_if2.py
@@ -1,8 +1,19 @@
-# Задание 2_if
 def strings_checkin(string12, string2):
-    if type(string12) == str and type(string2) == str:
-        return 1
-    else:
-        return 2
 
-print(strings_checkin('name1', 'name2'))
+    if type(string12) == str and type(string2) == str:
+        print(1)
+    else:
+        return 0
+
+    if string12 == string2:
+        print('Строки одинаковые')
+    elif len(string12) >= len(string2):
+        print('Не одинаковые и первая строка больше или равна второй')
+    elif string2 == 'learn':
+        print(3)
+
+    return 'Проверка закончена'
+
+print(strings_checkin('neme1233', 'learn'))
+print(strings_checkin(1, 'learn'))
+
